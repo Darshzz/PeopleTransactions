b314b0abea0b52b2737983dd957181117015b8c9
Darshan Mothreja <darshan.mothreja@gmail.com>
2022-07-13 09:23:20 +0530

    Initial setup of code. Dependencies added fro RxSwift and Swinject

---
+++ PeopleTransactions/TransactionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeopleTransactions
+//
+//  Created by Darsh on 13/07/22.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
